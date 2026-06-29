20512255279ac271389e019dfd7d03a702788540
hasancagri <hasancagridemiriz@gmail.com>
2026-06-29 23:45:30 +0300

    Revert "feat(catalog): per-tool scope authz on MCP tools, open transport for discovery"
    This reverts commit 971a03f8387aaf0fc96fdf286d607c9e25a2fea1.

--- src/services/catalog/Catalog.Api/Domains/Products/ProductMcpTools.cs
+++ src/services/catalog/Catalog.Api/Domains/Products/ProductMcpTools.cs
@@ -2,11 +2,11 @@ using System.ComponentModel;
 using Catalog.Api.Domains.Products.Features.Commands;
 using Catalog.Api.Domains.Products.Features.Queries;
 using Common;
-using Common.Utils.Constants;
 using ModelContextProtocol.Server;
 using Shared.Enums;
 
 namespace Catalog.Api.Domains.Products;
+
 
 [McpServerToolType]
 public static class GetProductMcpTool
@@ -16,16 +16,9 @@ public static class GetProductMcpTool
     public static Task<FeatureObjectResultModel<GetProductById.ProductResponse>> GetProductAsync(
         [Description("Urunun Id'si")] Guid id,
         IMessageBus bus,
-        IHttpContextAccessor http,
         CancellationToken ct)
-    {
-        if (http.HttpContext?.User.HasScope(AuthorizationScopes.CatalogRead) != true)
-            return Task.FromResult(FeatureObjectResultModel<GetProductById.ProductResponse>.Error(
-                new MessageItem { Code = "unauthorized_scope" }));
-
-        return bus.InvokeAsync<FeatureObjectResultModel<GetProductById.ProductResponse>>(
+        => bus.InvokeAsync<FeatureObjectResultModel<GetProductById.ProductResponse>>(
             new GetProductById.GetProductByIdQuery(id), ct);
-    }
 }
 
 [McpServerToolType]
@@ -37,16 +30,9 @@ public static class GetProductByNameMcpTool
         [Description("Aranacak urun adi (kismi eslesme yeterli)")] string name,
         [Description("Donecek azami sonuc sayisi (1-20, varsayilan 5)")] int? limit,
         IMessageBus bus,
-        IHttpContextAccessor http,
         CancellationToken ct)
-    {
-        if (http.HttpContext?.User.HasScope(AuthorizationScopes.CatalogRead) != true)
-            return Task.FromResult(FeatureObjectResultModel<List<GetProductById.ProductResponse>>.Error(
-                new MessageItem { Code = "unauthorized_scope" }));
-
-        return bus.InvokeAsync<FeatureObjectResultModel<List<GetProductById.ProductResponse>>>(
+        => bus.InvokeAsync<FeatureObjectResultModel<List<GetProductById.ProductResponse>>>(
             new GetProductByName.GetProductByNameQuery(name, limit ?? 5), ct);
-    }
 }
 
 [McpServerToolType]
@@ -63,16 +49,9 @@ public static class CreateProductMcpTool
         [Description("Urun gorsel URL'si (opsiyonel)")] string? imageUrl,
         [Description("Baslangic stok adedi")] int initialStock,
         IMessageBus bus,
-        IHttpContextAccessor http,
         CancellationToken ct)
-    {
-        if (http.HttpContext?.User.HasScope(AuthorizationScopes.CatalogWrite) != true)
-            return Task.FromResult(FeatureObjectResultModel<CreateProduct.CreateProductResponse>.Error(
-                new MessageItem { Code = "unauthorized_scope" }));
-
-        return bus.InvokeAsync<FeatureObjectResultModel<CreateProduct.CreateProductResponse>>(
+        => bus.InvokeAsync<FeatureObjectResultModel<CreateProduct.CreateProductResponse>>(
             new CreateProduct.CreateProductCommand(name, description, price, sku, brand, imageUrl, initialStock), ct);
-    }
 }
 
 [McpServerToolType]
@@ -89,16 +68,9 @@ public static class UpdateProductMcpTool
         [Description("Marka: Apple=1, Samsung=2, Sony=3, Nike=4, Adidas=5, Lenovo=6, Dell=7")] BrandType brand,
         [Description("Urun gorsel URL'si (opsiyonel)")] string? imageUrl,
         IMessageBus bus,
-        IHttpContextAccessor http,
         CancellationToken ct)
-    {
-        if (http.HttpContext?.User.HasScope(AuthorizationScopes.CatalogWrite) != true)
-            return Task.FromResult(FeatureObjectResultModel<UpdateProduct.UpdateProductResponse>.Error(
-                new MessageItem { Code = "unauthorized_scope" }));
-
-        return bus.InvokeAsync<FeatureObjectResultModel<UpdateProduct.UpdateProductResponse>>(
+        => bus.InvokeAsync<FeatureObjectResultModel<UpdateProduct.UpdateProductResponse>>(
             new UpdateProduct.UpdateProductCommand(id, name, description, price, sku, brand, imageUrl), ct);
-    }
 }
 
 [McpServerToolType]
@@ -109,14 +81,7 @@ public static class DeleteProductMcpTool
     public static Task<FeatureObjectResultModel<DeleteProduct.DeleteProductResponse>> DeleteProductAsync(
         [Description("Silinecek urunun Id'si")] Guid id,
         IMessageBus bus,
-        IHttpContextAccessor http,
         CancellationToken ct)
-    {
-        if (http.HttpContext?.User.HasScope(AuthorizationScopes.CatalogWrite) != true)
-            return Task.FromResult(FeatureObjectResultModel<DeleteProduct.DeleteProductResponse>.Error(
-                new MessageItem { Code = "unauthorized_scope" }));
-
-        return bus.InvokeAsync<FeatureObjectResultModel<DeleteProduct.DeleteProductResponse>>(
+        => bus.InvokeAsync<FeatureObjectResultModel<DeleteProduct.DeleteProductResponse>>(
             new DeleteProduct.DeleteProductCommand(id), ct);
-    }
 }
